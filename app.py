43e8b3bff1c466de00e3b5ff78553fd770b0d554
root <root@localhost.localdomain>
2016-06-19 11:14:19 +0800

    update some code

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 #!coding=utf-8
 # author 92ez.com
-# last modify 2015-08-23 22:03:40
 
-import re
-import os
-import web
-import sys
-import json
-import psutil
-import signal
-import MySQLdb
 import webbrowser
 import subprocess
-
+import signal
+import serial
+import time
+import json
+import sys
+import web
+import re
+import os
 
 urls = (
 	"/","index",
+	"/killdev","killDev",
 	"/getUSB","getUSB",
-	"/getProcess","getProcess",
 	"/getSMS","getSMS",
 	"/download","download",
+	"/readARFCN","readARFCN",
 	"/getARFCN","getARFCN",
+	"/smartARFCN","smartARFCN",
 	"/doSniffer","doSniffer",
 	"/stopSniffer","killProcess"
 )
@@ -33,66 +33,86 @@ render = web.template.render('static',cache = False)
 class index:
 	def GET(self):
 		subprocess.Popen("rm -rf *.dat",shell = True)
-		subprocess.Popen("service mysql restart",shell = True)
-		subprocess.Popen("killall ccch_scan cell_log osmocon 2>/dev/null",shell = True)
-
 		return render.index("static")
+
+class killDev:
+	def POST(self):
+		subprocess.Popen("killall ccch_scan cell_log osmocon 2>/dev/null",shell = True)
+		return json.dumps({"res":0}) 
 
 class getUSB:
 	def POST(self):
 		global USBLIST
 		USBLIST = []
-		for i in range(50):
-			if os.path.exists('/dev/ttyUSB'+str(i)):
-				USBLIST.append(i)
+
+		subPro2 = subprocess.Popen('ls /dev | grep ttyUSB', shell=True,stdout = subprocess.PIPE)
+		subPro2.wait()
+		ttylog = subPro2.communicate()
+
+		ttyusbList = ttylog[0].split('\n')
+
+		del ttyusbList[len(ttyusbList)-1]
+
+		for u in range(len(ttyusbList)):
+			USBLIST.append('/dev/'+ttyusbList[u])
+
 		return json.dumps({"total":len(USBLIST),"rows":USBLIST})
-
-class getProcess:
-	def POST(self):
-		processList = psutil.pids()
-
-		haveToSeeProcess = []
-		
-		for psIndex in range(len(processList)):
-			currentProcess = psutil.Process(processList[psIndex]) 
-			if(currentProcess.name() == 'ccch_scan'):
-				haveToSeeProcess.append(currentProcess.name())
-			if(currentProcess.name() == 'cell_log'):
-				haveToSeeProcess.append(currentProcess.name())
-			if(currentProcess.name() == 'osmocon'):
-				haveToSeeProcess.append(currentProcess.name())
-			if(currentProcess.name() == 'mysqld'):
-				haveToSeeProcess.append(currentProcess.name())
-		
-		print haveToSeeProcess
 
 class download:
 	def POST(self):
-		deviceId = web.input().get('deviceId')
+		global OSMOCOM_L2
 
-		downloadCommand = ['xterm','-e','./osmocon','-m','c123xor','-s','/tmp/osmocom_l2_'+ str(deviceId) ,'-p','/dev/ttyUSB'+ str(deviceId),'./layer1.compalram.bin']
-		downloadProcess = subprocess.Popen(downloadCommand,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
+		OSMOCOM_L2 = []
+		sw = serial.Serial(USBLIST[0],9600)
+		keyPort = ['10','11','12','14','15','16','17','19']
+		del USBLIST[0]
+
+		for dev in USBLIST:
+			try:
+				downloadCommand = ['xterm','-e',sys.path[0]+'/osmocombb_x64/osmocon','-m','c123','-s','/tmp/osmocom_l2_'+ str(USBLIST.index(dev)) ,'-p',dev,sys.path[0]+'/osmocombb_x64/layer1.compalram.bin']
+				downloadProcess = subprocess.Popen(downloadCommand,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
+				OSMOCOM_L2.append('/tmp/osmocom_l2_'+ str(USBLIST.index(dev)))
+				time.sleep(1)
+			except Exception,e:
+				return json.dumps({"res":-1,"msg":str(e)})
+
+		for port in keyPort:
+			try:
+				sw.write(port+'|200')
+				time.sleep(2)
+			except Exception,e:
+				return json.dumps({"res":-1,"msg":str(e)})
 		return json.dumps({"res":0})
+
+class readARFCN:
+	def POST(self):
+		arfcnlist = []
+		for line in open(sys.path[0]+"/arfcn.log"): 
+			arfcnlist.append(line.replace('\n',''))
+		return json.dumps({"res":0,"rows":arfcnlist[:-1],"date":arfcnlist[-1]})
 
 class getARFCN:
 	def POST(self):
-		deviceId = web.input().get('deviceId')
-		cellLogshell = ["./cell_log","-s","/tmp/osmocom_l2_"+str(deviceId),"-O"];
-		arfcnScan = subprocess.Popen(cellLogshell,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
-		scanlog = arfcnScan.communicate()
-		arfcnScan.wait()
-		scanloginfo = ";".join(scanlog)
-		scanbase = re.findall(r"ARFCN\=[^)]+\)",scanloginfo)
-		return json.dumps({"arfcn":scanbase,"total":len(scanbase)})
+		try:
+			cellLogshell = [sys.path[0]+"/osmocombb_x64/cell_log","-s","/tmp/osmocom_l2_0","-O"];
+			arfcnScan = subprocess.Popen(cellLogshell,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
+			scanlog = arfcnScan.communicate()
+			arfcnScan.wait()
+			scanloginfo = ";".join(scanlog)
+			scanbase = re.findall(r"ARFCN\=[^)]+\)",scanloginfo)
+		except Exception,e:
+			return json.dumps({"res":-1,"msg":str(e)})
+		return json.dumps({"res":0,"arfcn":scanbase,"total":len(scanbase)})
+
+class smartARFCN:
+	def POST(self):
+		return json.dumps({"res":0})
 
 class doSniffer:
- 	def POST(self):
- 		arfcnId = web.input().get('arfcnId')
-
-		sniffcommand = ["xterm","-e","./ccch_scan","-s","/tmp/osmocom_l2_0","-i","127.0.0.1","-a",arfcnId]
-
+	def POST(self):
+		arfcnId = web.input().get('arfcnId')
+		sniffcommand = ["xterm","-e",sys.path[0]+"/osmocombb_x64/ccch_scan","-s","/tmp/osmocom_l2_1","-i","127.0.0.1","-a",arfcnId]
 		snifferProcess =  subprocess.Popen(sniffcommand,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
-
 		return json.dumps({"res":0,"pid":snifferProcess.pid})
 
 class killProcess:
@@ -107,8 +127,8 @@ class getSMS:
 		page = int(web.input().get("page"))
 		rows = int(web.input().get("rows"))
 		limitCount = (page-1)*rows
-		data = mysql.query("SELECT * FROM sms_data limit "+str(limitCount)+","+str(rows)+";")
-		length = mysql.query("SELECT id FROM sms_data")
+		# data = mysql.query("SELECT * FROM sms_data limit "+str(limitCount)+","+str(rows)+";")
+		# total = mysql.query("SELECT id FROM sms_data")
 		SMSStr =[]
 		for row in data:
 			thisMsg = row['sms_message']
@@ -119,33 +139,7 @@ class getSMS:
 			thisId = str(row['id'])
 			SMSStr.append({"content":thisMsg,"id":thisId,"time":thisTime,"type":thisType,"to":thisCenter,"from":thisTel})
 
-		return '{"rows":'+  json.dumps(SMSStr)+',"total":'+ str(len(length))+'}'
-
-class Database:
-	host = '127.0.0.1'
-	user = 'root'
-	password = 'root'
-	db = 'smshack'
-	charset = 'utf8'
-
-	def __init__(self):
-		self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db,charset=self.charset)
-		self.cursor = self.connection.cursor()
- 
-	def insert(self, query):
-		try:
-				self.cursor.execute(query)
-				self.connection.commit()
-		except:
-				self.connection.rollback()
- 
-	def query(self, query):
-		cursor = self.connection.cursor( MySQLdb.cursors.DictCursor )
-		cursor.execute(query)
-		return cursor.fetchall()
- 
-	def __del__(self):
-		self.connection.close()
+		return json.dumps({"rows":SMSStr,"total":100})
 				
 if __name__ == "__main__":
 	webbrowser.open("http://localhost:"+sys.argv[1], new=0, autoraise=True)
--- static/res/js/index.js
+++ static/res/js/index.js
@@ -8,11 +8,66 @@ var hacksms = {
 			$('#devCount').text(data.total);
 			$('#devList').empty();
 			for(var dev = 0;dev < data.total;dev++){
-				$('#devList').append('<li><button class="btn btn-success">ttyUSB0</button></li>');
+				$('#devList').append('<li><button class="btn btn-success">'+ data.rows[dev] +'</button></li>');
 			}
 
-			if(data.total < 1){
+			if(data.total < 2){
 				$('#devList').html('<li><font color=red>未发现可用设备</font></li>');
+			}
+		},"json");
+	},
+	killdev:function(){
+		$.post('/killdev', {}, function(data, textStatus, xhr) {
+			if(data.res == 0){
+				alert('操作成功!');
+			}else{
+				alert('发生异常:'+data.msg);
+			}
+		},"json");
+	},
+	download:function(){
+		$.post('/download', {}, function(data, textStatus, xhr) {
+			if(data.res == 0){
+				alert('操作成功!');
+			}else{
+				alert('发生异常:'+data.msg);
+			}
+		},"json");
+	},
+	readARFCN:function(){
+		$.post('/readARFCN', {}, function(data, textStatus, xhr) {
+			if(data.res == 0){
+				$('#arfcnList').find('tbody').empty();
+				for(var arfcn = 0;arfcn < data.rows.length;arfcn++){
+					$('#arfcnList').find('tbody').append(
+						'<tr>'+
+                            '<td>'+ arfcn +'</td>'+
+                            '<td>'+ data.rows[arfcn] +'</td>'+
+                            '<td>'+ data.date +'</td>'+
+                            '<td><button class="btn btn-primary" onclick="hacksms.sniff('+ data.rows[arfcn].split(' ')[0].split('=')[1] +')">嗅探</button></td>'+
+                        '</tr>'
+					);
+				}
+			}else{
+				alert('发生异常:'+data.msg);
+			}
+		},"json");
+	},
+	scanARFCN:function(type){
+		$.post(type == 1?'/getARFCN':'/smartARFCN', {}, function(data, textStatus, xhr) {
+			if(data.res == 0){
+				alert(JSON.stringify(data));
+			}else{
+				alert('发生异常:'+data.msg);
+			}
+		},"json");
+	},
+	sniff:function(arfcn){
+		$.post('/doSniffer', {"arfcnId":arfcn}, function(data, textStatus, xhr) {
+			if(data.res == 0){
+				alert('操作成功! PID: '+data.pid);
+			}else{
+				alert('发生异常:'+data.msg);
 			}
 		},"json");
 	}
